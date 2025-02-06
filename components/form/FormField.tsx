3d765f202c77f0af41765f21d14e12812de78240
sikiru <sikirurazak1@gmail.com>
2025-02-06 14:13:34 +0100

    setup reusable form field

--- components/form/FormField.tsx
+++ components/form/FormField.tsx
@@ -1,9 +1,42 @@
-import React from "react";
-import { View } from "react-native";
-import { Controller } from "react-hook-form";
+import {
+  Control,
+  Controller,
+  ControllerRenderProps,
+  FieldPath,
+  FieldValues,
+} from "react-hook-form";
+import { View, Text } from "react-native";
+import * as Yup from "yup";
 
-const FormField = () => {
-  return <View></View>;
+const FormField = <T extends Yup.AnyObjectSchema>({
+  control,
+  name,
+  showError = true,
+  children,
+}: {
+  control: Control<Yup.InferType<T>>;
+  name: FieldPath<Yup.InferType<T>>;
+  showError?: boolean;
+  children: ({
+    field,
+  }: {
+    field: ControllerRenderProps<FieldValues, string>;
+  }) => React.ReactElement;
+}) => {
+  return (
+    <View>
+      <Controller
+        control={control}
+        name={name}
+        render={({ fieldState }) => (
+          <>
+            {children}
+            {showError && <Text>{fieldState.error?.message}</Text>}
+          </>
+        )}
+      />
+    </View>
+  );
 };
 
 export default FormField;
