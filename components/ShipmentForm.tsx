bd76b66fa573c7aa51d608a84aef6c3d7f75bd7d
sikiru <sikirurazak1@gmail.com>
2025-02-06 18:41:11 +0100

    update shipment form

--- components/ShipmentForm.tsx
+++ components/ShipmentForm.tsx
@@ -6,73 +6,231 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
-import { useCallback, useState } from "react";
-import { FloatingLabelInput } from "@/components/FloatingLabelInput";
-import { LoginValue } from "@/types";
-import { router } from "expo-router";
+import { useState } from "react";
 import { useAppContext } from "@/context/AppContext";
+import { useForm } from "react-hook-form";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { FloatingLabelInput } from "@/components/FloatingLabelInput";
+import { router } from "expo-router";
 import axios from "@/api/axios";
 import { ThemedView } from "./ThemedView";
+import * as FormMeta from "@/components/form/validators/create-shipment";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormField from "./form/FormField";
+import PhoneInput, {
+  ICountry,
+  isValidPhoneNumber,
+} from "react-native-international-phone-number";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
+import uuid from "react-native-uuid";
+import { shipmentTypeList } from "@/constants";
+import { shipmentType } from "@/types";
 
 export default function ShipmentForm() {
-  const {} = useAppContext();
-  const [value, setValue] = useState<LoginValue>({ usr: "", pwd: "" });
+  const { validationSchema, initialValues, Fields } = FormMeta;
+  const { isInternetReachable } = useNetInfo();
+  const { createShipment } = useAppContext();
+  const { control, handleSubmit, formState, setError, setValue, reset } =
+    useForm({
+      resolver: yupResolver(validationSchema),
+      defaultValues: initialValues,
+      reValidateMode: "onChange",
+    });
   const [loading, setLoading] = useState(false);
-  const onChangeText = useCallback((key: keyof LoginValue, value: string) => {
-    setValue((prev) => ({ ...prev, [key]: value }));
-  }, []);
-  const isFormValid = useCallback(() => {
-    return value.usr !== "" && value.pwd !== "";
-  }, [value]);
+  const [selectedCountry, setSelectedCountry] = useState<{
+    sender?: ICountry | undefined;
+    receiver?: ICountry | undefined;
+  }>();
+
   const showErrorAlert = (message: string) => {
     Alert.alert("Error", message, [{ text: "OK" }], { cancelable: false });
   };
-  const handleLogin = () => {
-    setLoading(true);
-    axios
-      .get<string, { data: Record<string, string>[] }>("/users")
-      .then((res) => res.data)
-      .then((users) => {
-        // setUsername(users[Math.random() * 10].name);
-        router.replace("/(tabs)");
-      })
-      .catch((err) => {
-        showErrorAlert(
-          err?.message || err.data?.message || "Something went wrong"
-        );
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+  const handleCreateAction = (data: Yup.InferType<typeof validationSchema>) => {
+    try {
+      if (isInternetReachable) {
+        setLoading(true);
+        axios
+          .get<string, { data: Record<string, string>[] }>("/users") //simulate network request
+          .then(() => {
+            createShipment({
+              ...data,
+              status: "in-progress",
+              id: uuid.v4(),
+              total_fees: 3000,
+              type: data.type as shipmentType,
+            });
+          })
+          .catch((err) => {
+            throw Error(err);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      } else {
+        createShipment({
+          ...data,
+          status: "pending",
+          id: uuid.v4(),
+          total_fees: 3000,
+          type: data.type as shipmentType,
+        });
+      }
+      reset();
+      router.back();
+    } catch (error: unknown) {
+      showErrorAlert((error as Error)?.message || "Something went wrong");
+    }
   };
   return (
     <ThemedView style={styles.container}>
       <View style={styles.formView}>
         <ThemedText style={styles.headerText} type="title">
-          Login
+          Create Shipment
         </ThemedText>
-        <ThemedText style={styles.formText} type="default">
-          Please enter your First, Last name and your phone number in order to
-          register
-        </ThemedText>
-        <FloatingLabelInput
-          onChangeText={(value) => onChangeText("usr", value)}
-          value={value.usr}
-          label="Email"
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.type}
+          render={({ field: { value } }) => (
+            <SegmentedControl
+              onChange={(e) => {
+                setValue(
+                  Fields.type,
+                  shipmentTypeList[e.nativeEvent.selectedSegmentIndex]
+                    .value as shipmentType,
+                  { shouldValidate: true, shouldTouch: true, shouldDirty: true }
+                );
+              }}
+              values={shipmentTypeList.map((e) => e.label)}
+              selectedIndex={shipmentTypeList.findIndex(
+                (e) => e.value === value
+              )}
+            />
+          )}
         />
-        <FloatingLabelInput
-          onChangeText={(value) => onChangeText("pwd", value)}
-          value={value.pwd}
-          label="Password"
-          secureTextEntry
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.sender_name}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <FloatingLabelInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value as string}
+              label="Sender Name"
+            />
+          )}
+        />
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.pickup_address}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <FloatingLabelInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value as string}
+              label="Pickup Address"
+            />
+          )}
+        />
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.sender_phone}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <PhoneInput
+              placeholder="Sender Phone Number"
+              onBlur={() => {
+                if (
+                  selectedCountry?.sender &&
+                  !isValidPhoneNumber(value, selectedCountry.sender)
+                ) {
+                  setError(Fields.sender_phone, {
+                    message: "invalid phone number",
+                  });
+                } else if (
+                  selectedCountry?.sender &&
+                  isValidPhoneNumber(value, selectedCountry.sender)
+                ) {
+                  setError(Fields.sender_phone, { message: "" });
+                }
+                onBlur();
+              }}
+              onChangePhoneNumber={onChange}
+              onChangeSelectedCountry={(country: ICountry) => {
+                setSelectedCountry((prev) => ({
+                  ...(prev ?? {}),
+                  sender: country,
+                }));
+              }}
+              selectedCountry={selectedCountry?.sender}
+              value={value}
+            />
+          )}
+        />
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.receiver_name}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <FloatingLabelInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value as string}
+              label="Receiver Name"
+            />
+          )}
+        />
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.destination_address}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <FloatingLabelInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value as string}
+              label="Destination Address"
+            />
+          )}
+        />
+        <FormField<typeof validationSchema>
+          control={control}
+          name={Fields.receiver_phone}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <PhoneInput
+              placeholder="Receiver Phone Number"
+              onBlur={() => {
+                if (
+                  selectedCountry?.receiver &&
+                  !isValidPhoneNumber(value, selectedCountry.receiver)
+                ) {
+                  setError(Fields.receiver_phone, {
+                    message: "invalid phone number",
+                  });
+                } else if (
+                  selectedCountry?.receiver &&
+                  isValidPhoneNumber(value, selectedCountry.receiver)
+                ) {
+                  setError(Fields.receiver_phone, { message: "" });
+                }
+                onBlur();
+              }}
+              onChangePhoneNumber={onChange}
+              onChangeSelectedCountry={(country: ICountry) => {
+                setSelectedCountry((prev) => ({
+                  ...(prev ?? {}),
+                  receiver: country,
+                }));
+              }}
+              selectedCountry={selectedCountry?.receiver}
+              value={value}
+            />
+          )}
         />
       </View>
       <Pressable
-        onPress={handleLogin}
-        disabled={!isFormValid()}
+        onPress={handleSubmit(handleCreateAction)}
+        disabled={!formState.isValid}
         style={[
           styles.button,
-          { backgroundColor: isFormValid() ? "#2F50C1" : "#EAE7F2" },
+          { backgroundColor: formState.isValid ? "#2F50C1" : "#EAE7F2" },
         ]}
       >
         {loading ? (
@@ -81,10 +239,10 @@ export default function ShipmentForm() {
           <ThemedText
             style={[
               styles.buttonText,
-              { color: isFormValid() ? "#FFFFFF" : "#A7A3B3" },
+              { color: formState.isValid ? "#FFFFFF" : "#A7A3B3" },
             ]}
           >
-            Create Shipment
+            Submit
           </ThemedText>
         )}
       </Pressable>
@@ -101,13 +259,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFF",
   },
   headerText: {
-    color: "#000000",
+    color: "#2F50C1",
     fontSize: 34,
     fontWeight: 800,
-    marginBottom: "10%",
-  },
-  formText: {
-    color: "#757281",
+    marginBottom: 8,
+    textAlign: "center",
   },
   formView: {
     display: "flex",
@@ -116,7 +272,7 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     justifyContent: "center",
     width: "100%",
-    gap: 15,
+    gap: 1,
   },
   button: {
     width: "100%",
